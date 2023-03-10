b78d04bc8a60f1241959bb450a9cc1c337cde2da
Andreas Hennig <andreashennig@miniOne.fritz.box>
2023-03-10 12:06:25 +0100

    Finished chapter 6

--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -1,11 +1,17 @@
 mod map;
-mod player;
 mod map_builder;
 mod camera;
+mod components;
+mod spawner;
+mod systems;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
-    
+
+    pub use legion::*;
+    pub use legion::world::SubWorld;
+    pub use legion::systems::CommandBuffer;
+
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
 
@@ -13,27 +19,52 @@ mod prelude {
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
 
     pub use crate::map::*;
-    pub use crate::player::*;
     pub use crate::map_builder::*;
     pub use crate::camera::*;
+
+    pub use crate::components::*;
+
+    pub use crate::spawner::*;
+
+    pub use crate::systems::*;
 }
+
+use std::thread::spawn;
 
 use prelude::*;
 
 struct State {
-    map: Map,
-    player: Player,
-    camera: Camera,
+    ecs : World,
+    resources : Resources,
+    systems : Schedule,
+    // map: Map,
+    // camera: Camera,
 }
 
 impl State {
     fn new() -> Self {
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
+        spawn_player(&mut ecs, map_builder.player_start);
+
+        map_builder.rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
+
+        resources.insert(map_builder.map);
+        resources.insert(Camera::new(map_builder.player_start));
+
         Self { 
-            map: map_builder.map,
-            player: Player::new(map_builder.player_start),
-            camera: Camera::new(map_builder.player_start)
+            ecs,
+            resources,
+            systems: build_scheduler()
+            // map: map_builder.map,
+            //player: Player::new(map_builder.player_start),
+            // camera: Camera::new(map_builder.player_start)
         }
     }
 }
@@ -45,12 +76,17 @@ impl GameState for State {
         ctx.set_active_console(1);
         ctx.cls();
 
+        self.resources.insert(ctx.key);
+        self.systems.execute(&mut self.ecs, &mut self.resources);
+
+        render_draw_buffer(ctx).expect("Render error");
+
         // update
-        self.player.update(ctx, &self.map, &mut self.camera);
+        //self.player.update(ctx, &self.map, &mut self.camera);
 
         // render
-        self.map.render(ctx, &self.camera);
-        self.player.render(ctx, &self.camera);
+        //self.map.render(ctx, &self.camera);
+        //self.player.render(ctx, &self.camera);
     }
 }
 
